3fcba026701b6298f8e045e132876ccb20bf9433
Atmois <atmois@atmois.com>
2024-04-02 12:26:45 +0100

    more tidy up

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// This base of this code is AI generated & docs code (I know fuckall about JS serverside lmao), so it's very sketchy and not representitive of my programming abilites 
+// This base of this code is AI generated & docs code (I know fuckall about JS serverside lmao), so it's very sketchy and not representitive of my programming abilites
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
@@ -15,22 +15,22 @@ server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
 
 app.post("/plot-data", (req, res) => {
-    const itemID = req.body.itemID;
-    console.log("Received POST request for /plot-data \n Item ID:", itemID);
-    const filePath = path.join(__dirname, `./ItemValues/${itemID}.csv`);
+  const itemID = req.body.itemID;
+  console.log("Received POST request for /plot-data for the item", itemID);
+  const filePath = path.join(__dirname, `./ItemValues/${itemID}.csv`);
 
-    fs.readFile(filePath, "utf8", (err, data) => {
-        if (err) {
-            console.error(err);
-            return res.status(500).send("Error reading file");
-        }
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Error reading file");
+    }
 
-        const lines = data.split("\n");
-        const dates = lines[0].split(",").slice(1);
-        const prices = lines[1].split(",").slice(1).map(parseFloat);
+    const lines = data.split("\n");
+    const dates = lines[0].split(",").slice(1);
+    const prices = lines[1].split(",").slice(1).map(parseFloat);
 
-        const plotData = dates.map((date, index) => ({ date, value: prices[index] }));
+    const plotData = dates.map((date, index) => ({ date, value: prices[index] }));
 
-        res.json(plotData);
-    });
+    res.json(plotData);
+  });
 });
